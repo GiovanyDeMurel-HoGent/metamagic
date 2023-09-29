58e8844100d6efe142beb915688e939653247839
GiovanyDeMurel-HoGent <giovany.demurel@student.hogent.be>
2023-09-29 19:47:56 +0000

    refactored DeckBox component useEffect

--- metamagic-app/src/features/decks/DeckBox.tsx
+++ metamagic-app/src/features/decks/DeckBox.tsx
@@ -1,24 +1,22 @@
-import { Card, Deck } from "metamagic-types"
-import { useState } from "react"
-import axios from "axios"
-import CardsList from "../cards/CardsList"
-import CardImage from "../cards/CardImage"
+import { Card, Deck } from "metamagic-types";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CardsList from "../cards/CardsList";
+import CardImage from "../cards/CardImage";
 
-
-export function DeckBox(deck:Deck) {
-  const [loading, setLoading] = useState(false)
-  const [clicked, setClicked] = useState(false)
+export function DeckBox(deck: Deck ) {
+  const [loading, setLoading] = useState(true);
+  const [clicked, setClicked] = useState(false);
   const [cards, setCards] = useState<Array<Card>>([]);
-  const [selectedCard, setSelectedCard] = useState<Card>()
+  const [selectedCard, setSelectedCard] = useState<Card>();
 
-  async function handleClick() {
+  useEffect(() => {
+    if(loading && clicked)
+    getCards();
+  }, [loading, clicked]);
+
+  function handleClick() {
     setClicked(!clicked);
-    if (!loading && clicked) {
-      setCards([]);
-    } else if (!loading) {
-      setLoading(true);
-      await getCards();
-    }
   }
 
   async function getCards() {
@@ -30,24 +28,23 @@ export function DeckBox(deck:Deck) {
       setLoading(false);
     } catch (error) {
       console.error('Error fetching data:', error);
-      setLoading(false);
     }
   }
 
   return (
-    <div >
-    <h2>{deck.name}</h2>
-    {deck.description && <p>Description: {deck.description}</p>}
-    <p>Commander Name: {deck.commander.name}</p>
-    <p>Commander Color Identity: {deck.commander.color_identity}</p>
-    {selectedCard?.image_uris &&
-      <CardImage 
-        {...selectedCard}/>}
-    <button onClick={handleClick}>
-      {clicked ? "Hide Cards" : "Show Cards"}
-    </button>
-    {clicked && loading && <p>Loading...</p>}
-    {clicked && !loading && <CardsList cards={cards} setSelectedCard={setSelectedCard} />}
-  </div>
-  )
+    <div>
+      <h2>{deck.name}</h2>
+      {deck.description && <p>Description: {deck.description}</p>}
+      <p>Commander Name: {deck.commander.name}</p>
+      <p>Commander Color Identity: {deck.commander.color_identity}</p>
+      {selectedCard?.image_uris && (
+        <CardImage {...selectedCard} />
+      )}
+      <button onClick={handleClick}>
+        {clicked ? "Hide Cards" : "Show Cards"}
+      </button>
+      {clicked && loading && <p>Loading...</p>}
+      {clicked && !loading && <CardsList cards={cards} setSelectedCard={setSelectedCard} />}
+    </div>
+  );
 }
