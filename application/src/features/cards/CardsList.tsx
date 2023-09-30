85d7605b42650048aa64c0e94261f8f4bedf70e2
GiovanyDeMurel-HoGent <giovany.demurel@student.hogent.be>
2023-09-30 13:22:13 +0000

    implemented incrementing and decrementing card amount in deckPage cards state

--- application/src/features/cards/CardsList.tsx
+++ application/src/features/cards/CardsList.tsx
@@ -1,28 +1,39 @@
-
 import { Card } from "metamagic-types";
 
 interface CardsListProps {
-  cards: Card[]
-  setSelectedCard: (card:Card) => void,
-  onRemoveCard: (cardToRemoveId:string) => void,
+  cards: Card[];
+  setSelectedCard: (card: Card) => void;
+  onRemoveCard: (cardToRemoveId: string) => void;
+  onDecrementAmount: (cardToDecrementId: string, amount?: number) => void;
+  onIncrementAmount: (cardToIncrementId: string, amount?: number) => void;
 }
-export default function CardsList({ cards, setSelectedCard, onRemoveCard}: CardsListProps) {
-
+export default function CardsList({
+  cards,
+  setSelectedCard,
+  onRemoveCard,
+  onDecrementAmount,
+  onIncrementAmount,
+}: CardsListProps) {
   return (
-      <ul>
-      {
-      cards.map((card) => (
-        <li key={card.id}  >
-          <a onClick={() => {
-          setSelectedCard(card);
-        }}>
-           {card.name} {card.amount}
+    <ul>
+      {cards.map((card) => (
+        <li key={card.id}>
+          <a
+            onClick={() => {
+              setSelectedCard(card);
+            }}
+          >
+            {card.name} {card.amount}
           </a>
-
+          <button onClick={() => onDecrementAmount(card.id, card.amount)}>
+            +
+          </button>
+          <button onClick={() => onIncrementAmount(card.id, card.amount)}>
+            -
+          </button>
           <button onClick={() => onRemoveCard(card.id)}>remove</button>
         </li>
-      )) 
-      }
-      </ul>
-  )
+      ))}
+    </ul>
+  );
 }
--- application/src/pages/DeckPage.tsx
+++ application/src/pages/DeckPage.tsx
@@ -1,31 +1,36 @@
-import { Card, Deck } from "metamagic-types"
-import { useParams, useLocation } from "react-router-dom"
-import { useCallback, useEffect, useState } from "react"
-import axios from "axios"
-import CardsList from "../features/cards/CardsList"
-import CardImage from "../features/cards/CardImage"
+import { Card, Deck } from "metamagic-types";
+import { useParams, useLocation } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import CardsList from "../features/cards/CardsList";
+import CardImage from "../features/cards/CardImage";
 
 export default function DeckPage() {
-  const { id } = useParams()
-  const location = useLocation()
-  const [deck, setDeck] = useState<Deck>({} as Deck)
-  const [cards, setCards] = useState<Array<Card>>([])
-  const [selectedCard, setSelectedCard] = useState<Card>()
-  const [loading, setLoading] = useState(true)
+  const { id } = useParams();
+  const location = useLocation();
+  const [deck, setDeck] = useState<Deck>({} as Deck);
+  const [cards, setCards] = useState<Array<Card>>([]);
+  const [selectedCard, setSelectedCard] = useState<Card>();
+  const [loading, setLoading] = useState(true);
 
   //set deck with uselocation.state if navvigating via react-router Link
   //fetch deck first if entering url
   const getDeckData = useCallback(async () => {
     try {
       if (location.state) {
-        setDeck(location.state)
+        setDeck(location.state);
       } else {
-        const deckResponse = await axios.get(`http://localhost:3000/api/decks/${id}`);
+        const deckResponse = await axios.get(
+          `http://localhost:3000/api/decks/${id}`
+        );
         setDeck(deckResponse.data);
       }
-      const cardsResponse = await axios.get(`http://localhost:3000/api/decks/${id}/cards`);
+      const cardsResponse = await axios.get(
+        `http://localhost:3000/api/decks/${id}/cards`
+      );
+      console.log(cardsResponse.data);
       setCards(cardsResponse.data);
-  
+
       setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -33,14 +38,48 @@ export default function DeckPage() {
   }, [id, location.state]);
 
   useEffect(() => {
-    if (loading) getDeckData()
-  }, [getDeckData, loading])
+    if (loading) getDeckData();
+  }, [getDeckData, loading]);
 
-  const handleRemoveCard = (cardToRemoveId:string) => {
-    // Filter out the card to be removed from the cards array
+  const handleRemoveCard = (cardToRemoveId: string) => {
     const updatedCards = cards.filter((card) => card.id !== cardToRemoveId);
     setCards(updatedCards);
   };
+
+  function handleIncrementAmount(
+    cardToDecrementId: string,
+    amount?: number
+  ): void {
+    if (amount) {
+      const updatedAmount = amount + 1;
+      const updatedCards = cards.map((card) => {
+        if (card.id === cardToDecrementId) {
+          return { ...card, amount: updatedAmount };
+        }
+        return card;
+      });
+      setCards(updatedCards);
+    }
+  }
+  function handleDecrementAmount(
+    cardToDecrementId: string,
+    amount?: number
+  ): void {
+    if (amount) {
+      const updatedAmount = amount - 1;
+      if (updatedAmount == 0) {
+        handleRemoveCard(cardToDecrementId);
+      } else {
+        const updatedCards = cards.map((card) => {
+          if (card.id === cardToDecrementId) {
+            return { ...card, amount: updatedAmount };
+          }
+          return card;
+        });
+        setCards(updatedCards);
+      }
+    }
+  }
 
   return (
     <>
@@ -51,9 +90,15 @@ export default function DeckPage() {
           <p>Commander Name: {deck.commander.name}</p>
           <p>Commander Color Identity: {deck.commander.color_identity}</p>
           {selectedCard && <CardImage {...selectedCard} />}
-          <CardsList cards={cards} setSelectedCard={setSelectedCard} onRemoveCard={handleRemoveCard} />
+          <CardsList
+            cards={cards}
+            setSelectedCard={setSelectedCard}
+            onRemoveCard={handleRemoveCard}
+            onDecrementAmount={handleIncrementAmount}
+            onIncrementAmount={handleDecrementAmount}
+          />
         </div>
       )}
     </>
-  )
+  );
 }
